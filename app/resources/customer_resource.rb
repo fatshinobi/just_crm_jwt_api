07911ec7a1b1be9344fe1d1a7d9c09ca5937937e
Rem <r3mnik@gmail.com>
2026-06-22 11:01:27 +0300

    Add avatars for customers

--- app/resources/customer_resource.rb
+++ app/resources/customer_resource.rb
@@ -1,3 +1,12 @@
 class CustomerResource < BaseResource
+  include Rails.application.routes.url_helpers
   attributes :id, :name, :about
+
+  attribute :avatar_url do |customer|
+    if customer.avatar.attached?
+      rails_blob_url(customer.avatar, only_path: false)
+    else
+      nil
+    end
+  end
 end
--- app/resources/customer_show_resource.rb
+++ app/resources/customer_show_resource.rb
@@ -1,7 +1,16 @@
 class CustomerShowResource < BaseResource
+  include Rails.application.routes.url_helpers
   attributes :id, :name, :email, :phone, :address, :about, :user_id
 
   attribute :user_name do |customer|
     customer.user&.name
+  end
+
+  attribute :avatar_url do |customer|
+    if customer.avatar.attached?
+      rails_blob_url(customer.avatar, only_path: false)
+    else
+      nil
+    end
   end
 end
