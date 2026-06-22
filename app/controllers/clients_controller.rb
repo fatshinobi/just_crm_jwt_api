6093febf6602840f8064e9ca65782d1c6f87042a
Rem <r3mnik@gmail.com>
2026-06-22 18:31:44 +0300

    Create client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,4 +3,19 @@ class ClientsController < ApplicationController
     clients = ClientResource.new(Client.all)
     render json: clients.to_json, status: :ok
   end
+
+  def create
+    client = Client.new(client_params)
+    if client.save
+      render json: ClientResource.new(client), status: :created
+    else
+      render json: { errors: client.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def client_params
+    params.permit(:name, :about, :user_id, :avatar, :email, :phone, :social)
+  end
 end
