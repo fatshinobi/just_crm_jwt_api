ad0da409c7c05cb7ff0b1fbb5f64fcbcf67efa3b
Rem <r3mnik@gmail.com>
2026-06-23 22:38:53 +0300

    Add update action for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,5 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: [ :show, :update ]
   def index
     clients = ClientResource.new(Client.all)
     render json: clients.to_json, status: :ok
@@ -7,18 +8,29 @@ class ClientsController < ApplicationController
   def create
     client = Client.new(client_params)
     if client.save
-      render json: ClientResource.new(client), status: :created
+      render json: ClientShowResource.new(client), status: :created
     else
       render json: { errors: client.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def show
-    client = Client.find(params[:id])
-    render json: ClientShowResource.new(client).to_json, status: :ok
+    render json: ClientShowResource.new(@client).to_json, status: :ok
+  end
+
+  def update
+    if @client.update(client_params)
+      render json: ClientShowResource.new(@client).to_json, status: :ok
+    else
+      render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
 
   def client_params
     params.permit(:name, :about, :user_id, :avatar, :email, :phone, :social)
