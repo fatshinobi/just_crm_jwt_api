70cc9b0996d1c86ff863a360d7251ab125100619
Rem <r3mnik@gmail.com>
2026-09-17 22:25:15 +0300

    Implement update and show actions for attachments

--- app/resources/client_attachment_element_resource.rb
+++ app/resources/client_attachment_element_resource.rb
@@ -1,3 +1,7 @@
 class ClientAttachmentElementResource < BaseResource
   attributes :id, :description, :attachment_type
+
+  attribute :customer_id do |attachment|
+    attachment.attachable_id
+  end
 end
