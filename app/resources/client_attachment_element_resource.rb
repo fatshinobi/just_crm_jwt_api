3b55828c2e6fb4e639117417174f8fa60fc78a7e
Rem <r3mnik@gmail.com>
2026-09-17 22:48:10 +0300

    Show file name for attachment

--- app/resources/client_attachment_element_resource.rb
+++ app/resources/client_attachment_element_resource.rb
@@ -4,4 +4,8 @@ class ClientAttachmentElementResource < BaseResource
   attribute :customer_id do |attachment|
     attachment.attachable_id
   end
+
+  attribute :uploaded_file_name do |attachment|
+    attachment.uploaded_file.filename.to_s if attachment.uploaded_file.attached?
+  end
 end
